5550ed0f3ab94bfb45cc188b2451b5eefa84760a
elvira <elvirapedregal@hotmail.es>
2020-07-21 15:49:12 +0200

    improving toogle

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import React, { useState } from 'react';
 
 const Wraped = styled.div`
 width:100%;
@@ -14,16 +15,21 @@ const TopNav = styled.div`
  }
 background-color: #333;
 overflow: hidden;
+
 >a {
   float: left;
   display: block;
   color: #f2f2f2;
+color: ${props=>props.open === 'close'? 'red':'green'};
+
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
   font-size: 17px;
 }
 `;
+
+
 
 const Logo = styled.div`
   width: 60px;
@@ -34,17 +40,35 @@ const Logo = styled.div`
 `;
 
 const Toogle = styled.div`
-  @media (min-width: 768px) {
-    display: none; 
-    background: url(/img/toogle.png);
+  @media (max-width: 768px) {
+    display: flex; 
   }
+  display: none; 
+  height: auto;  
+  width: 30px; 
+  color: red; 
+  background: url(/img/toogle.png)
+  
 `
 
 function Navbar() {
+  //declaro una funcion de estado
+  const [toogleClass,setToogleClass] = useState("close"); 
+  //aplico cambios al state a traves de la funcion handleToogle que emite el evento onClick
+  console.log(toogleClass)
+  function handleToogle(){
+    alert("hola")
+    if(toogleClass==="close"){
+      setToogleClass("open")
+    }
+    if(toogleClass==="open"){
+      setToogleClass("close")
+    }
+  }
   return <Wraped>
             <Logo>
             </Logo>
-            <TopNav>
+            <TopNav className={toogleClass}  open={toogleClass}>
               <a href="#home" class="active">Home</a>
               <a href="#news">News</a>
               <a href="#contact">Contact</a>
@@ -53,8 +77,7 @@ function Navbar() {
               <i class="fa fa-bars"></i>
               </a>
             </TopNav>
-            <Toogle>
-              <a onClick=""></a>
+            <Toogle onClick={handleToogle}>
             </Toogle>
           </Wraped>;
 }
